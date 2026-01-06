2e53d88ad5d4f37dc1fff5c6c9627888276e5a12
rustaceanrob <rob.netzke@gmail.com>
2026-01-06 14:21:25 +0000

    Initial open PR summary

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,58 @@
 use chrono::{DateTime, Months, Utc};
-use octocrab::{models::{pulls::MergeableState, IssueState, Repository}, params::repos::Sort, Octocrab};
-use std::sync::Arc;
+use octocrab::{Octocrab, models::IssueState};
+use serde::Deserialize;
+use std::{fs::File, io::BufReader, sync::Arc};
 
-const LOGIN: &str = "rustaceanrob";
+#[derive(Deserialize, Debug)]
+struct Query {
+    username: String,
+    repositories: Vec<(String, String)>,
+}
 
 #[tokio::main]
 async fn main() {
     let quarter = Utc::now() - Months::new(3);
+    let file = File::open("./config.json").expect("could not find config.json");
+    let buf_reader = BufReader::new(file);
+    let query: Query = serde_json::from_reader(buf_reader).unwrap();
     let octocrab = octocrab::instance();
-    let user = octocrab.users(LOGIN);
-    let repos = user
-        .repos()
-        .sort(Sort::Updated)
-        .per_page(1)
-        .send()
-        .await
-        .unwrap();
-    for repo in repos.into_iter() {
-        build_repository_summary(octocrab.clone(), repo, quarter).await;
+    for (owner, repo) in query.repositories {
+        build_repository_summary(
+            octocrab.clone(),
+            quarter,
+            query.username.clone(),
+            owner,
+            repo,
+        )
+        .await;
     }
 }
 
-async fn build_repository_summary(octo: Arc<Octocrab>, repo: Repository, quarter: DateTime<Utc>) {
-    let source = repo.parent.as_deref().unwrap_or(&repo);
-    let owner = source.owner.as_ref().unwrap().login.clone();
-    let repo_name = source.name.clone();
-    println!("Repository {repo_name}");
-    println!("Owner {owner}");
+async fn build_repository_summary(
+    octo: Arc<Octocrab>,
+    quarter: DateTime<Utc>,
+    username: String,
+    owner: String,
+    repo: String,
+) {
     let prs = octo
-        .pulls(owner, repo_name)
+        .pulls(owner, repo)
         .list()
-        .per_page(50)
+        .per_page(100)
         .send()
         .await
         .unwrap();
     let filtered_on_user = prs
         .into_iter()
-        .filter(|pr| pr.user.is_some()) 
-        .map(|pr| {
-            println!("User login: {}", pr.user.clone().unwrap().login);
-            pr
-        })
-        .filter(|pr| pr.user.clone().unwrap().login == LOGIN)
+        .filter(|pr| pr.user.is_some())
+        .filter(|pr| pr.user.clone().unwrap().login == username)
         .filter(|pr| pr.created_at.is_some())
         .filter(|pr| pr.created_at.unwrap() > quarter);
-    for pr in filtered_on_user { 
+    for pr in filtered_on_user {
         if let Some(text) = pr.title {
             println!("{text} #{}", pr.number);
         }
-        if let Some(text) = pr.body_text {
+        if let Some(text) = pr.body {
             println!("{text}");
         }
         if let Some(state) = pr.state {
@@ -61,8 +65,8 @@ async fn build_repository_summary(octo: Arc<Octocrab>, repo: Repository, quarter
         if let Some(comments) = pr.comments {
             println!("Comments {comments}");
         }
-        if let Some(mergable) = pr.mergeable_state {
-            println!("Merge status {mergable:?}");
+        if let Some(mergeable) = pr.mergeable_state {
+            println!("Merge status {mergeable:?}");
         }
     }
 }
